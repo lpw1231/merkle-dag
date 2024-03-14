dd083d25bd575ace6020eaa0d37ec7c48f27eb9f
lpw1231 <152594001+lpw1231@users.noreply.github.com>
2024-03-14 19:22:51 +0800

    dag.go

--- dag.go
+++ dag.go
@@ -1,6 +1,9 @@
 package merkledag
 
-import "hash"
+import (
+	"encoding/json"
+	"hash"
+)
 
 type Link struct {
 	Name string
@@ -14,6 +17,76 @@ type Object struct {
 }
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+	switch node.Type() {
+	case DIR:
+		file := node.(File)
+		tmp := StoreFile(store, file, h)
+		jsonMarshal, _ := json.Marshal(tmp)
+		hash := calculateHash(jsonMarshal, h)
+		return hash
+	case FILE:
+		dir := node.(Dir)
+		tmp := StoreDir(store, dir, h)
+		jsonMarshal, _ := json.Marshal(tmp)
+		hash := calculateHash(jsonMarshal, h)
+		return hash
+	}
+	panic("unknown node")
+}
+
+func calculateHash(data []byte, h hash.Hash) []byte {
+	h.Reset()
+	hash := h.Sum(data)
+	h.Reset()
+	return hash
+}
+
+func StoreFile(store KVStore, file File, h hash.Hash) *Object {
+	data := file.Bytes()
+	blob := Object{Data: data, Links: nil}
+	jsonMarshal, _ := json.Marshal(blob)
+	hash := calculateHash(jsonMarshal, h)
+	store.Put(hash, data)
+	return &blob
+}
+
+func StoreDir(store KVStore, dir Dir, h hash.Hash) *Object {
+	it := dir.It()
+	treeObject := &Object{}
+	for it.Next() {
+		n := it.Node() //当前目录下的node
+		switch n.Type() {
+		case FILE:
+			file := n.(File)
+			tmp := StoreFile(store, file, h)
+			jsonMarshal, _ := json.Marshal(tmp)
+			hash := calculateHash(jsonMarshal, h)
+			treeObject.Links = append(treeObject.Links, Link{
+				Hash: hash,
+				Size: int(file.Size()),
+				Name: file.Name(),
+			})
+			typeName := "link"
+			if tmp.Links == nil {
+				typeName = "blob"
+			}
+			treeObject.Data = append(treeObject.Data, []byte(typeName)...)
+		case DIR:
+			dir := n.(Dir)
+			tmp := StoreDir(store, dir, h)
+			jsonMarshal, _ := json.Marshal(tmp)
+			hash := calculateHash(jsonMarshal, h)
+			treeObject.Links = append(treeObject.Links, Link{
+				Hash: hash,
+				Size: int(dir.Size()),
+				Name: dir.Name(),
+			})
+			typeName := "tree"
+			treeObject.Data = append(treeObject.Data, []byte(typeName)...)
+		}
+	}
+	jsonMarshal, _ := json.Marshal(treeObject)
+	hash := calculateHash(jsonMarshal, h)
+	store.Put(hash, jsonMarshal)
+	return treeObject
 }
